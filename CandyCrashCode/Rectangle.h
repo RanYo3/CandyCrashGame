c7c69726ec3a9efcc28ad473cf679cd7b61b20c4
RanYo3 <ranyo3@gmail.com>
2015-01-18 20:06:09 +0200

    Added core code

---
+++ CandyCrashCode/Rectangle.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Shape.h"
+
+class Rectangle : public Shape
+{
+public:
+	Rectangle();
+	virtual ~Rectangle();
+
+private:
+
+};
+
+Rectangle::Rectangle()
+{
+}
+
+Rectangle::~Rectangle()
+{
+}
